cb8f6e4b38c504478e1dbc4d443353af8088fb0a
Justin <n.justinaro@gmail.com>
2024-02-28 13:29:10 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import Header from "./Components/Header"
+export default function App() {
+  return (
+    <Header/>
+  )
+}
---
+++ src/Components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return(
+        <div className="bg-[#2b2c37] text-white p-4 h-20">
+            
+        </div>
+    )
+}
