f816534d614888a112f11c5b7620b05b8b3411db
Justin <n.justinaro@gmail.com>
2024-03-05 16:36:01 -0500

    added toggle

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,8 +1,10 @@
 import boardicon from "../assets/board-icon.svg"
+import light from "../assets/light.svg"
+import dark from "../assets/dark.svg"
 
 export default function Sidebar() {
     return(
-        <div className="flex bg-[#2b2c37] w-64 flex-grow pt-3 pr-5 flex-col">
+        <div className="flex bg-[#2b2c37] w-64 flex-grow pt-3 pr-5 flex-col ">
             {/*All Boards*/}
             <div className="text-[#828fa3] text-xs font-bold p-5">
                 <h3 className="tracking-boardSpacing">ALL BOARDS <span>(1)</span></h3>
@@ -15,6 +17,11 @@ export default function Sidebar() {
                 </button>
             </div>
             {/*Light Dark Toggle*/}
+            <div className="flex justify-evenly bg-[#20212c] p-3 rounded-md items-center ml-3 ">
+                <img src={light} className="pt-1"></img>
+                <label><input type="checkbox" /></label>
+                <img src={dark} className="pt-1"></img>
+            </div>
             {/*Hide Sidebar*/}
         </div>
     )
